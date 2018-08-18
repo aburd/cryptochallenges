b343033ff30b8e02794502bd59b7141afae20e14
aburd <burdick.aaron@gmail.com>
2018-08-18 19:57:34 +0900

    Add code for second challenge

--- internal/util.go
+++ internal/util.go
@@ -1,9 +1,9 @@
 package util
 
 import (
-	"log"
-	"encoding/hex"
 	"encoding/base64"
+	"encoding/hex"
+	"log"
 )
 
 func HexToBase64(str string) string {
@@ -12,4 +12,17 @@ func HexToBase64(str string) string {
 		log.Fatal("Error decoding string")
 	}
 	return base64.StdEncoding.EncodeToString(decoded)
+}
+
+func HexToFixedXor(str string, against string) string {
+	decoded, err := hex.DecodeString(str)
+	decoded_against, err := hex.DecodeString(against)
+	if err != nil {
+		log.Fatal("Error decoding string")
+	}
+	var xord []byte
+	for i := 0; i < len(decoded); i++ {
+		xord = append(xord, decoded[i]^decoded_against[i])
+	}
+	return hex.EncodeToString(xord)
 }
--- level1/level1_test.go
+++ level1/level1_test.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-    "testing"
-    "github.com/aburd/cryptochallenge/internal"
+	"testing"
+
+	"github.com/aburd/cryptochallenge/internal"
 )
 
 func TestHexToBase64(t *testing.T) {
-    var (
-        original string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-        expected string = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-    )
+	var (
+		original string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+		expected string = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+	)
 	res := util.HexToBase64(original)
-    if res != expected {
-       t.Errorf("HexToBase64 was incorrect, got: %s, want: %s.", res, expected)
-    }
+	if res != expected {
+		t.Errorf("HexToBase64 was incorrect, got: %s, want: %s.", res, expected)
+	}
+}
+
+func TestHexToFixedXor(t *testing.T) {
+	var (
+		original string = "1c0111001f010100061a024b53535009181c"
+		against  string = "686974207468652062756c6c277320657965"
+		expected string = "746865206b696420646f6e277420706c6179"
+	)
+	res := util.HexToFixedXor(original, against)
+	if res != expected {
+		t.Errorf("HexToFixedXor was incorrect, got: %s, want: %s.", res, expected)
+	}
 }
